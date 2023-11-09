02e6e22ecda71dd03b0b6584d12162d5dac3908c
Dimitri Heinrich <dimi-mail@gmx.de>
2023-11-09 16:21:18 +0100

    corrected code in Java code style

--- src/homework25/task01/Human.java
+++ src/homework25/task01/Human.java
@@ -1,10 +1,11 @@
 package homework25.task01;
 
 public class Human {
-String name;
-int age;
 
-void introduce(){
-  System.out.println("Привет! Меня зовут " + name + ", мне " + age + " лет.");
-}
+  String name;
+  int age;
+
+  void introduce() {
+    System.out.println("Привет! Меня зовут " + name + ", мне " + age + " лет.");
+  }
 }
