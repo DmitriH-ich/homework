fd0aecaa63ee06b376b7c4a15f92283e2255affe
Dimitri Heinrich <dimi-mail@gmx.de>
2023-12-17 15:20:06 +0100

    Lesson 39 task02

--- src/homework38/Customer.java
+++ src/homework38/Customer.java
@@ -1,6 +1,7 @@
 package homework38;
 
-/*public class Customer extends Person {
+
+public class Customer extends Person {
 
     private double money;
 
@@ -9,12 +10,37 @@ package homework38;
         this.money = money;
     }
 
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public boolean pay(double amount) {
+        if (money < amount) {
+            System.out.println("Customer: I dont have enough money!");
+            return false;
+        }
+        System.out.println("Customer: Sure, ill pay " + amount);
+        money -= amount;
+        return true;
+    }
 
     public void buyProduct(Seller seller, String productName, int amount) {
-        double price = seller.getPrice(productName, amount);
-        //TODO: finish rest of transaction in this method
-
+        System.out.println("Customer: I want to buy "
+                + amount + " " + productName + " please");
+        double price = seller.getProductPrice(productName, amount);
+        if (price == -1) {
+            return;
+        }
+        boolean success = pay(price);
+        if (!success) {
+            return;
+        }
+        seller.sell(productName, amount);
+        System.out.println("Customer: thanks!");
     }
 }
 
- */
---
+++ src/homework39/task02/Developer.java
@@ -0,0 +1,13 @@
+package homework39.task02;
+
+public class Developer extends Employee {
+
+    public Developer(String name, double salary) {
+        super(name, "Developer", salary);
+    }
+
+    public void work() {
+        System.out.println("Developer " + name
+                + " is working on some feature...");
+    }
+}
